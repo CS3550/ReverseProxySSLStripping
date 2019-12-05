4e760c06d8b871db4e17b05ed121077bf2d2fcca
UNO User <unouser@unomaha.edu>
2019-12-05 09:59:00 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+ 
+//
+// From: https://www.npmjs.com/package/http-proxy
+//
+var proxy = httpProxy.createProxyServer({ssl: {
+    key: fs.readFileSync('private', 'utf8'),
+    cert: fs.readFileSync('certificate', 'utf8')
+  }});
+
+ 
+var server = http.createServer(function(req, res) {
+  console.log("Proxying");
+  proxy.web(req, res, {
+    target: 'http://127.0.0.1:8080'
+  });
+});
+ 
+console.log("listening on port 443")
+server.listen(443);
