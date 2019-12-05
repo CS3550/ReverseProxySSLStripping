8dfe305acd7672336b6bb977737b94d087d6ec5d
B Ricks <bricks@unomaha.edu>
2019-12-05 10:44:43 -0600

    Add reference to https

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
-const fs=require("fs");
- 
-//
-// From: https://www.npmjs.com/package/http-proxy
-//
-var proxy = httpProxy.createProxyServer({ssl: {
-    key: fs.readFileSync('private', 'utf8'),
-    cert: fs.readFileSync('certificate', 'utf8')
-  }});
+const https = require('https');
+const httpProxy = require('http-proxy');
+const fs = require("fs");
 
- 
-var server = http.createServer(function(req, res) {
-  console.log("Proxying");
-  proxy.web(req, res, {
-    target: 'http://127.0.0.1:8080'
+var proxy = httpProxy.createProxyServer();
+
+var server = https.createServer(
+  {
+    key: fs.readFileSync('private'),//This is the private key
+    cert: fs.readFileSync('certificate'), //This is the certificate (public key)
+  }, function (req, res) {
+    console.log("Proxying to 8080");
+    proxy.web(req, res, {
+      target: 'http://127.0.0.1:8080',
+    }
+    );
   });
-});
- 
+
+
 console.log("listening on port 443")
 server.listen(443);
